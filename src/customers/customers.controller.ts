fa309d93eaa94ba0c83650282c7effae465082ca
sebastien baudet <seb.baudet76@gmail.com>
2023-01-25 18:08:17 +0100

    fin de crud user

--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -1,17 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CustomersService } from './customers.service';
 import { CustomerDto } from './dto/customer.dto';
 
 @Controller('customers')
 export class CustomersController {
-    constructor(private customerService: CustomersService){}
-    
-    @Post('addCustomer')
-    addCustomer(@Body() Datas: CustomerDto) {    
-        console.log(Datas);
-           
-      return this.customerService.addCustomer(Datas)
-    }
+  constructor(private customerService: CustomersService) {}
 
+  @Post('addCustomer')
+  addCustomer(@Body() Datas: CustomerDto) {
+    console.log(Datas);
+
+    return this.customerService.addCustomer(Datas);
+  }
+
+  @Get('getCustomers')
+  findAlluser() {
+    return this.customerService.GetAll();
+  }
 }
-
